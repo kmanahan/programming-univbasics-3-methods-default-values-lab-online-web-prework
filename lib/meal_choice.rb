5731c6e5e0e3c087ea860ffbe8eea1df47e8ac3a
congenial-language-8324 <kelly.a.manahan@gmail.com>
2019-09-22 13:05:20 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,17 @@
 # Your code here
-def
-puts meal_choice"A plate of #{protein}with#{veg1}and#{veg2}."
+def meal_choice(veg1, veg2, protein = "meat")
+<<<<<<< HEAD
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
+=======
+  puts "What a nutritious meal!" + "\n" + "A plate of #{protein} with #{veg1} and #{veg2}."
+>>>>>>> b78ed73af004fba127fcece5c763f6f7ea5b7747
 end
+
+meal_choice("spinach", "beans")
+meal_choice("spinach", "beans","beef")
+  
   
 
   
